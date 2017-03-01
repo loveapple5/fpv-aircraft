6a86032643d2b8a96b6a0c11ecf9961013d95b99
小五 <164660718@qq.com>
2017-03-02 01:21:01 +0800

    飞控参数设置

--- app/src/main/java/com/dji/FPVDemo/fragment/FlightControllerSettingFragment.java
+++ app/src/main/java/com/dji/FPVDemo/fragment/FlightControllerSettingFragment.java
@@ -7,14 +7,19 @@ import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.dji.FPVDemo.FPVDemoApplication;
 import com.dji.FPVDemo.R;
+
+import java.util.ArrayList;
 
 import dji.common.error.DJIError;
 import dji.common.flightcontroller.DJIFlightControllerCurrentState;
@@ -24,6 +29,8 @@ import dji.common.flightcontroller.DJIFlightControllerSmartGoHomeStatus;
 import dji.common.flightcontroller.DJILocationCoordinate2D;
 import dji.common.flightcontroller.DJILocationCoordinate3D;
 import dji.common.flightcontroller.DJIFlightFailsafeOperation;
+import dji.common.remotecontroller.DJIRCControlMode;
+import dji.common.remotecontroller.DJIRCControlStyle;
 import dji.common.util.DJICommonCallbacks;
 import dji.sdk.flightcontroller.DJIFlightController;
 import dji.sdk.flightcontroller.DJIFlightLimitation;
@@ -40,11 +47,23 @@ public class FlightControllerSettingFragment extends Fragment {
 
     private Button btnGoHome;
     private SwitchCompat scChangeFlightMode;
+
     private EditText etHomeHeight;
     private Button btnHomeHeight;
 
     private EditText etMaxFlightHeight;
     private Button btnMaxFlightHeight;
+
+    private EditText etMaxFlightRadius;
+    private Button btnMaxFlightRadius;
+
+    private SwitchCompat scNoviceMode;
+    private Spinner spFailSafe;
+    private SwitchCompat scSmartGoHome;
+    private SwitchCompat scLED;
+    private SwitchCompat scVisionPositioning;
+
+    private ArrayList failSafeModeList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,6 +81,47 @@ public class FlightControllerSettingFragment extends Fragment {
         etMaxFlightHeight = (EditText) root.findViewById(R.id.et_max_flight_height);
         btnMaxFlightHeight = (Button) root.findViewById(R.id.btn_max_flight_height);
         btnMaxFlightHeight.setOnClickListener(new MaxFlightHeightClickListener());
+
+        etMaxFlightRadius = (EditText) root.findViewById(R.id.et_max_flight_radius);
+        btnMaxFlightRadius = (Button) root.findViewById(R.id.btn_max_flight_radius);
+        btnMaxFlightRadius.setOnClickListener(new MaxFlightRadiusClickListener());
+
+        scNoviceMode = (SwitchCompat) root.findViewById(R.id.sc_novice_mode);
+        scNoviceMode.setOnCheckedChangeListener(new NoviceModeScListener());
+
+        scSmartGoHome = (SwitchCompat) root.findViewById(R.id.sc_smart_go_home);
+        scSmartGoHome.setOnCheckedChangeListener(new SmartGoHomeScListener());
+        scLED = (SwitchCompat)root.findViewById(R.id.sc_led);
+        scLED.setOnCheckedChangeListener(new LEDScListener());
+        scVisionPositioning = (SwitchCompat) root.findViewById(R.id.sc_vision_positioning);
+        scVisionPositioning.setOnCheckedChangeListener(new VisionPositioningScListener());
+
+        spFailSafe = (Spinner) root.findViewById(R.id.sc_fail_safe);
+        //数据
+        failSafeModeList = new ArrayList<String>();
+        failSafeModeList.add("悬停");
+        failSafeModeList.add("下降");
+        failSafeModeList.add("返航");
+
+        //适配器
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, failSafeModeList);
+        //设置样式
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        spFailSafe.setAdapter(adapter);
+        spFailSafe.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                DJIFlightFailsafeOperation op = DJIFlightFailsafeOperation.find(i);
+                djiFlightController.setFlightFailsafeOperation(op, null);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         return root;
     }
 
@@ -110,7 +170,6 @@ public class FlightControllerSettingFragment extends Fragment {
         @Override
         public void onClick(View view) {
             String text = etMaxFlightHeight.getText().toString();
-            DJIFlightControllerCurrentState state = djiFlightController.getCurrentState();
             float height = Float.parseFloat(text);
             if (height >= 20 && height <= 500) {
                 DJIFlightLimitation limitation = djiFlightController.getFlightLimitation();
@@ -129,6 +188,70 @@ public class FlightControllerSettingFragment extends Fragment {
         }
     }
 
+    private class MaxFlightRadiusClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String text = etMaxFlightRadius.getText().toString();
+            float height = Float.parseFloat(text);
+            if (height >= 20 && height <= 500) {
+                DJIFlightLimitation limitation = djiFlightController.getFlightLimitation();
+                limitation.setMaxFlightRadius(height, new MaxFlightRadiusCallback());//最大距离
+                limitation.setMaxFlightRadiusLimitationEnabled(true, new MaxFlightRadiusCallback());
+            } else {
+                Toast.makeText(getActivity(), R.string.max_flight_radius_toast, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class MaxFlightRadiusCallback implements DJICommonCallbacks.DJICompletionCallback {
+
+        @Override
+        public void onResult(DJIError djiError) {
+
+        }
+    }
+
+    private class NoviceModeScListener implements CompoundButton.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            if (checked) {
+                DJIFlightLimitation limitation = djiFlightController.getFlightLimitation();
+                limitation.setMaxFlightHeight(30, new MaxFlightHeightCallback());//最大升限
+                limitation.setMaxFlightRadius(30, new MaxFlightRadiusCallback());//最大距离
+                limitation.setMaxFlightRadiusLimitationEnabled(true, new MaxFlightRadiusCallback());
+            }
+
+        }
+    }
+
+    private class SmartGoHomeScListener implements CompoundButton.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            DJIFlightControllerCurrentState state = djiFlightController.getCurrentState();
+            state.getSmartGoHomeStatus().setAircraftShouldGoHome(checked);//设置自动返航
+//        state.getSmartGoHomeStatus().setBatteryPercentageNeededToGoHome();//设置自动返航电量
+        }
+    }
+
+    private class LEDScListener implements CompoundButton.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            djiFlightController.setLEDsEnabled(checked, null);//LED开关
+        }
+    }
+
+    private class VisionPositioningScListener implements CompoundButton.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            djiFlightController.getIntelligentFlightAssistant().setVisionPositioningEnabled(checked ,null);//视觉定位
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -140,8 +263,6 @@ public class FlightControllerSettingFragment extends Fragment {
         //--------------------------B2飞控参数设置--------------------------------------
         djiAircraft = (DJIAircraft) FPVDemoApplication.getProductInstance();
         djiFlightController = djiAircraft.getFlightController();
-        DJIFlightControllerCurrentState state = djiFlightController.getCurrentState();
-        djiFlightController.setFlightFailsafeOperation();//通讯中断后措施
         //返航点位置
         //state.setHomepoint();
 //        state.setGoHomeHeight();//返航高度
@@ -155,6 +276,5 @@ public class FlightControllerSettingFragment extends Fragment {
 //
 //        djiFlightController.setLEDsEnabled();//LED开关
 //        djiFlightController.getIntelligentFlightAssistant().setVisionPositioningEnabled();//视觉定位
-
     }
 }
