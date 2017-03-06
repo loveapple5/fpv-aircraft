a2ba7c386afe7bba05167f083ca88ec136a9e6cc
loveapple5 <164660718@qq.com>
2017-03-06 23:35:11 +0800

    NTSC/PAL选择

--- app/src/main/java/com/dji/FPVDemo/fragment/CameraVideoFragment.java
+++ app/src/main/java/com/dji/FPVDemo/fragment/CameraVideoFragment.java
@@ -29,6 +29,7 @@ public class CameraVideoFragment extends Fragment {
 
     private Spinner spVideoFormat;
     private Spinner spVideoResolve;
+    private Spinner spVideoStandard;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +47,15 @@ public class CameraVideoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_camera_video, container, false);
         spVideoFormat = (Spinner) view.findViewById(R.id.sp_video_format);
+        spVideoResolve = (Spinner) view.findViewById(R.id.sp_video_resolve);
+        spVideoStandard = (Spinner) view.findViewById(R.id.sp_video_standard);
+        initVideoFormat();
+        initVideoResolve();
+        initVideoStandard();
+        return view;
+    }
+
+    private void initVideoFormat() {
         //数据
         ArrayList<String> RCFormatList = new ArrayList<String>();
         RCFormatList.add("MOV");
@@ -70,8 +80,9 @@ public class CameraVideoFragment extends Fragment {
 
             }
         });
+    }
 
-        spVideoResolve = (Spinner) view.findViewById(R.id.sp_video_resolve);
+    private void initVideoResolve() {
         //数据
         ArrayList<String> resolveList = new ArrayList<String>();
         resolveList.add("2704×1520/24fps");
@@ -115,8 +126,32 @@ public class CameraVideoFragment extends Fragment {
 
             }
         });
-
-        return view;
     }
 
+    private void initVideoStandard() {
+        //数据
+        ArrayList<String> RCFormatList = new ArrayList<String>();
+        RCFormatList.add("PAL");
+        RCFormatList.add("NTSC");
+//        RCModeList.add("自定义");
+
+        //适配器
+        ArrayAdapter RCModeAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, RCFormatList);
+        //设置样式
+        RCModeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        spVideoFormat.setAdapter(RCModeAdapter);
+        spVideoFormat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                DJICameraSettingsDef.CameraVideoStandard standard = DJICameraSettingsDef.CameraVideoStandard.find(position);
+                djiCamera.setVideoStandard(standard, null);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
 }
