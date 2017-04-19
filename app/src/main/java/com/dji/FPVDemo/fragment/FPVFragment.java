e5e22c56ea0963d10e31f7722d48d3784b56b910
五爷 <164660718@qq.com>
2017-04-20 00:08:51 +0800

    添加fav页面UI

---
+++ app/src/main/java/com/dji/FPVDemo/fragment/FPVFragment.java
@@ -0,0 +1,22 @@
+package com.dji.FPVDemo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dji.FPVDemo.R;
+
+
+public class FPVFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fpv, container, false);
+        return view;
+    }
+}
