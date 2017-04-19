efa12dcd5c8cd62baaff961a2b09ca66a59a435e
五爷 <164660718@qq.com>
2017-04-20 00:09:22 +0800

    切换fpv/tpv

--- app/src/main/java/com/dji/FPVDemo/FPVActivity.java
+++ app/src/main/java/com/dji/FPVDemo/FPVActivity.java
@@ -8,13 +8,17 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
+import com.dji.FPVDemo.fragment.FPVFragment;
 import com.dji.FPVDemo.fragment.TPVFragment;
 
 public class FPVActivity extends FragmentActivity {
 
     private TPVFragment mTPVFragment;
+    private FPVFragment mFPVFragment;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +26,35 @@ public class FPVActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_fpv);
 
+        mFPVFragment = new FPVFragment();
+        mTPVFragment = new TPVFragment();
+
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
-        mTPVFragment = new TPVFragment();
         transaction.replace(R.id.layout_fpv_root, mTPVFragment);
         transaction.commit();
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_fpv, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.menu_fpv:
+                transaction.replace(R.id.layout_fpv_root, mFPVFragment);
+                transaction.commit();
+                break;
+            case R.id.menu_tpv:
+                transaction.replace(R.id.layout_fpv_root, mTPVFragment);
+                transaction.commit();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
