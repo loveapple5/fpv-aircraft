aad737bfcb4385f04f76151485e83eb961de863b
loveapple5 <164660718@qq.com>
2017-11-20 21:30:45 +0800

    添加找回的坐标转换相关代码

---
+++ app/src/main/java/com/dji/FPVDemo/util/CoordinateUtils.java
@@ -0,0 +1,89 @@
+package com.dji.FPVDemo.util;
+
+import java.util.Vector;
+
+public class CoordinateUtils {
+
+    //地球赤道半径（单位：米）
+    public static final double Re = 6378137;
+
+    //地球极地半径（单位：米）
+    public static final double Rp = 6356752;
+
+    //椭圆矫正参数
+    public static final double E = (Re * Re - Rp * Rp) / Re * Re;
+
+    public static double getRadiusByLatitude(double latitude) {
+        double sinLa = Math.sin(latitude);
+        double W = Math.sqrt(1 - E * E * sinLa * sinLa);
+        return Re / W;
+    }
+
+    /***
+     * 通过经纬度海拔获取地心坐标
+     */
+    public static Vector<Double> getGeoCoordinateByLBH(Vector<Double> LBH) {
+        double longitude = LBH.get(0);
+        double latitude = LBH.get(1);
+        double height = LBH.get(2);
+        double N = getRadiusByLatitude(latitude);
+
+        double X = (N + height) * Math.cos(latitude) * Math.sin(longitude);
+        double Y = (N * (1 - E * E) + height) * Math.sin(latitude);
+        double Z = (N + height) * Math.cos(latitude) * Math.cos(longitude);
+        Vector<Double> geoCoordinate = new Vector<>();
+        geoCoordinate.add(X);
+        geoCoordinate.add(Y);
+        geoCoordinate.add(Z);
+        return geoCoordinate;
+    }
+
+
+    /**
+     * 获取向量在地心坐标系中的方向角
+     * @param A
+     * @param B
+     * @return
+     */
+    public static Vector<Double> getAngleByVector(Vector<Double> A, Vector<Double> B) {
+        double X = B.get(0) - A.get(0);
+        double Y = B.get(1) - A.get(1);
+        double Z = B.get(2) - A.get(2);
+        double length = Math.sqrt(X * X + Y * Y + Z * Z);
+        double Ax = Math.acos(X / length);
+        double Ay = Math.acos(Y / length);
+        double Az = Math.acos(Z / length);
+        Vector<Double> angle = new Vector<>();
+        angle.add(Ax);
+        angle.add(Ay);
+        angle.add(Az);
+        return angle;
+    }
+
+
+    public static Vector<Double> getAngleByPRYAndLBH(Vector<Double> PRY, Vector<Double> LBH) {
+        double pitch = PRY.get(0);
+        double roll = PRY.get(1);
+        double yaw = PRY.get(2);
+
+        double longitude = LBH.get(0);
+        double latitude = LBH.get(1);
+        double height = LBH.get(2);
+
+        // x = cosL * cosP + sinL * (sinB * cosR + cosB * cosY)
+        double X = Math.cos(longitude) * Math.cos(pitch) + Math.sin(longitude) * (Math.sin(latitude) * Math.cos(roll) +  Math.cos(latitude) * Math.cos(yaw));
+        double Y = Math.cos(latitude) * Math.cos(roll) - Math.sin(latitude) * Math.cos(yaw);
+        double Z = -Math.sin(longitude) * Math.cos(pitch) + Math.cos(longitude) * (Math.sin(latitude) * Math.cos(roll) + Math.cos(latitude) * Math.cos(yaw));
+        Vector<Double> vector = new Vector<>();
+        vector.add(X);
+        vector.add(Y);
+        vector.add(Z);
+
+        Vector<Double> core = new Vector<>();
+        core.add(0d);
+        core.add(0d);
+        core.add(0d);
+        Vector angle = getAngleByVector(core, vector);
+        return angle;
+    }
+}
