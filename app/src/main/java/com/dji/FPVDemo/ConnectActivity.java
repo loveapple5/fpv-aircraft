4401e65ababda02732e3516bdd53c5d8137a9c08
五爷 <164660718@qq.com>
2017-12-11 23:47:23 +0800

    连接页跳转设置页

--- app/src/main/java/com/dji/FPVDemo/ConnectActivity.java
+++ app/src/main/java/com/dji/FPVDemo/ConnectActivity.java
@@ -1,6 +1,7 @@
 package com.dji.FPVDemo;
 
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,18 +13,20 @@ import android.widget.ImageView;
 
 import com.dji.FPVDemo.view.WaveView;
 
-public class ConnectActivity extends FragmentActivity {
+public class ConnectActivity extends FragmentActivity implements View.OnClickListener{
 
     private static final String TAG = ConnectActivity.class.getName();
 
     private WaveView wcConnect;
     private ImageView ivLauncher;
+    private View btnSetting;
 
     private Handler handler = new Handler();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);   //禁止锁屏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_connect);
         ivLauncher = (ImageView) findViewById(R.id.iv_launcher);
@@ -36,14 +39,26 @@ public class ConnectActivity extends FragmentActivity {
         wcConnect.setInterpolator(new LinearOutSlowInInterpolator());
         wcConnect.start();
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ivLauncher.setVisibility(View.GONE);
-            }
-        }, 3000);
+        btnSetting = findViewById(R.id.tv_setting);
+        btnSetting.setOnClickListener(this);
+
+        handler.postDelayed(hideLauncher, 3000);
     }
 
+    private Runnable hideLauncher = new Runnable() {
+        @Override
+        public void run() {
+            ivLauncher.setVisibility(View.GONE);
+        }
+    };
 
-
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.tv_setting:
+                Intent intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
