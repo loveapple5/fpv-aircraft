10d02cb45461915d22ab4ba6089543b21fb61ee5
五爷 <164660718@qq.com>
2017-12-13 23:22:29 +0800

    添加电池历史页UI

--- app/src/main/java/com/dji/FPVDemo/BatteryHistoryActivity.java
+++ app/src/main/java/com/dji/FPVDemo/BatteryHistoryActivity.java
@@ -1,27 +1,23 @@
 package com.dji.FPVDemo;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.WindowManager;
 
-public class BatteryActivity extends FragmentActivity implements View.OnClickListener {
+public class BatteryHistoryActivity extends FragmentActivity implements View.OnClickListener {
 
     private View btnBack;
-    private View btnHistory;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);   //禁止锁屏
 
-        setContentView(R.layout.activity_battery);
+        setContentView(R.layout.activity_battery_history);
 
         btnBack = findViewById(R.id.iv_back);
         btnBack.setOnClickListener(this);
-        btnHistory = findViewById(R.id.ll_battery_history);
-        btnHistory.setOnClickListener(this);
     }
 
     @Override
@@ -29,10 +25,6 @@ public class BatteryActivity extends FragmentActivity implements View.OnClickLis
         switch (v.getId()) {
             case R.id.iv_back:
                 onBackPressed();
-                break;
-            case R.id.ll_battery_history:
-                Intent history = new Intent(this, BatteryHistoryActivity.class);
-                startActivity(history);
                 break;
         }
     }
