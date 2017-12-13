1ccaffa36bc7f91b4ceab706ecbdcee1fc88779d
五爷 <164660718@qq.com>
2017-12-13 11:27:07 +0800

    添加飞行控制设置页UI

--- app/src/main/java/com/dji/FPVDemo/FCActivity.java
+++ app/src/main/java/com/dji/FPVDemo/FCActivity.java
@@ -1,26 +1,22 @@
 package com.dji.FPVDemo;
 
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
-public class SettingActivity extends FragmentActivity implements View.OnClickListener {
+
+public class FCActivity extends FragmentActivity implements View.OnClickListener {
 
     private View btnBack;
-    private View btnFc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_fc);
 
         btnBack = findViewById(R.id.iv_back);
         btnBack.setOnClickListener(this);
-        btnFc = findViewById(R.id.ll_fc);
-        btnFc.setOnClickListener(this);
     }
 
     @Override
@@ -28,10 +24,6 @@ public class SettingActivity extends FragmentActivity implements View.OnClickLis
         switch (v.getId()) {
             case R.id.iv_back:
                 finish();
-                break;
-            case R.id.ll_fc:
-                Intent fcIntent = new Intent(this, FCActivity.class);
-                startActivity(fcIntent);
                 break;
         }
     }
