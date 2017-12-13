6a58813f00ae224804d27d5df31409edcb3b9659
五爷 <164660718@qq.com>
2017-12-13 11:53:27 +0800

    添加飞行限制设置页面UI

--- app/src/main/java/com/dji/FPVDemo/FLActivity.java
+++ app/src/main/java/com/dji/FPVDemo/FLActivity.java
@@ -1,27 +1,23 @@
 package com.dji.FPVDemo;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
-
-public class FCActivity extends FragmentActivity implements View.OnClickListener {
+public class FLActivity extends FragmentActivity implements View.OnClickListener {
 
     private View btnBack;
-    private View btnFlightLimit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fc);
+        setContentView(R.layout.activity_fl);
 
         btnBack = findViewById(R.id.iv_back);
         btnBack.setOnClickListener(this);
-
-        btnFlightLimit = findViewById(R.id.ll_flight_limit);
-        btnFlightLimit.setOnClickListener(this);
     }
 
     @Override
@@ -29,10 +25,6 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
         switch (v.getId()) {
             case R.id.iv_back:
                 finish();
-                break;
-            case R.id.ll_flight_limit:
-                Intent flIntent = new Intent(this, FLActivity.class);
-                startActivity(flIntent);
                 break;
         }
     }
