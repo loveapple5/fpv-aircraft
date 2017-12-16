60a72248e25d5ea9633c1e86381cda98e15ba9f0
五爷 <164660718@qq.com>
2017-12-16 23:33:07 +0800

    添加电池设置页逻辑

--- app/src/main/java/com/dji/FPVDemo/BatteryActivity.java
+++ app/src/main/java/com/dji/FPVDemo/BatteryActivity.java
@@ -3,14 +3,61 @@ package com.dji.FPVDemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.dji.FPVDemo.util.StringUtils;
+
+import dji.common.battery.DJIBatteryState;
+import dji.common.error.DJIError;
+import dji.common.flightcontroller.DJIFlightControllerCurrentState;
+import dji.common.util.DJICommonCallbacks;
+import dji.sdk.battery.DJIBattery;
+import dji.sdk.flightcontroller.DJIFlightController;
+import dji.sdk.products.DJIAircraft;
 
 public class BatteryActivity extends FragmentActivity implements View.OnClickListener {
 
+    protected static final int MSG_BATTERY_STATUS = 1;
+    protected static final int MSG_BATTERY_DISCHARGE_TIME = 2;
+
     private View btnBack;
     private View btnHistory;
+
+    private TextView tvBatteryCurrentEnergy;
+    private TextView tvBatteryTemperature;
+    private TextView tvFlightTime;
+    private EditText etDischargeTime;
+
+    private DJIBattery djiBattery;
+
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_BATTERY_STATUS:
+                    String BatteryCurrentEnergy = msg.getData().getString("BatteryCurrentEnergy");
+                    tvBatteryCurrentEnergy.setText(BatteryCurrentEnergy);
+
+                    String BatteryTemperature = msg.getData().getString("BatteryTemperature");
+                    tvBatteryTemperature.setText(BatteryTemperature);
+                    break;
+                case MSG_BATTERY_DISCHARGE_TIME:
+                    int dischargeTime = msg.getData().getInt("BatteryDischargeDay");
+                    etDischargeTime.setText(String.valueOf(dischargeTime));
+                    break;
+            }
+        }
+
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +69,22 @@ public class BatteryActivity extends FragmentActivity implements View.OnClickLis
         btnBack.setOnClickListener(this);
         btnHistory = findViewById(R.id.ll_battery_history);
         btnHistory.setOnClickListener(this);
+
+        tvBatteryCurrentEnergy = (TextView) findViewById(R.id.tv_battery_energy);
+        tvBatteryTemperature = (TextView) findViewById(R.id.tv_battery_temperature);
+        tvFlightTime = (TextView) findViewById(R.id.tv_flight_time);
+        etDischargeTime = (EditText) findViewById(R.id.et_discharge_time);
+        etDischargeTime.setOnEditorActionListener(etListener);
+
+        DJIAircraft djiAircraft = (DJIAircraft) FPVDemoApplication.getProductInstance();
+        if (djiAircraft != null) {
+            djiBattery = djiAircraft.getBattery();
+            djiBattery.setBatteryStateUpdateCallback(new BatteryStateUpdateCallback());
+            DJIFlightController djiFlightController = djiAircraft.getFlightController();
+            DJIFlightControllerCurrentState state = djiFlightController.getCurrentState();
+            tvFlightTime.setText(StringUtils.getTime(state.getFlightTime()));
+            djiBattery.getSelfDischargeDay(dischargeDayCallback);
+        }
     }
 
     @Override
@@ -36,4 +99,55 @@ public class BatteryActivity extends FragmentActivity implements View.OnClickLis
                 break;
         }
     }
+
+    class BatteryStateUpdateCallback implements DJIBattery.DJIBatteryStateUpdateCallback {
+        @Override
+        public void onResult(DJIBatteryState djiBatteryState) {
+
+            Bundle bundle = new Bundle();
+            bundle.putString("BatteryCurrentEnergy", djiBatteryState.getCurrentEnergy() + "mAH");
+
+            String temperature = String.format("%.1f", djiBatteryState.getBatteryTemperature());
+            bundle.putString("BatteryTemperature", temperature + "℃");
+            Message msg = Message.obtain();
+            msg.what = MSG_BATTERY_STATUS;
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+
+        }
+    }
+
+    private DJICommonCallbacks.DJICompletionCallbackWith<Integer> dischargeDayCallback = new DJICommonCallbacks.DJICompletionCallbackWith<Integer>() {
+
+        @Override
+        public void onSuccess(Integer day) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("BatteryDischargeDay", day);
+            Message msg = Message.obtain();
+            msg.what = MSG_BATTERY_DISCHARGE_TIME;
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+        }
+
+        @Override
+        public void onFailure(DJIError djiError) {
+
+        }
+    };
+
+    private TextView.OnEditorActionListener etListener = new TextView.OnEditorActionListener() {
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            //按下确认的时候进行处理
+            if (EditorInfo.IME_ACTION_DONE == actionId) {
+                int id = v.getId();
+                if (id == R.id.et_discharge_time) {
+                    String time = etDischargeTime.getText().toString();
+                    djiBattery.setSelfDischargeDay(Integer.valueOf(time), null);
+                }
+            }
+            return false;
+        }
+    };
 }
--- app/src/main/java/com/dji/FPVDemo/util/StringUtils.java
+++ app/src/main/java/com/dji/FPVDemo/util/StringUtils.java
@@ -10,7 +10,7 @@ public class StringUtils {
 
         if (cameraISO == DJICameraSettingsDef.CameraISO.Auto) {
             ISO = "AUTO";
-        }else if (cameraISO == DJICameraSettingsDef.CameraISO.ISO_100) {
+        } else if (cameraISO == DJICameraSettingsDef.CameraISO.ISO_100) {
             ISO = "100";
         } else if (cameraISO == DJICameraSettingsDef.CameraISO.ISO_200) {
             ISO = "200";
@@ -98,5 +98,55 @@ public class StringUtils {
         }
 
         return "";
+    }
+
+    //根据秒数转化为分秒  00:00
+    public static String getTime(int second) {
+        if (second < 10) {
+            return "00:0" + second;
+        }
+        if (second < 60) {
+            return "00:" + second;
+        }
+        if (second < 3600) {
+            int minute = second / 60;
+            second = second - minute * 60;
+            if (minute < 10) {
+                if (second < 10) {
+                    return "0" + minute + ":0" + second;
+                }
+                return "0" + minute + ":" + second;
+            }
+            if (second < 10) {
+                return minute + ":0" + second;
+            }
+            return minute + ":" + second;
+        }
+        return "00:00";
+//        int hour = second / 3600;
+//        int minute = (second - hour * 3600) / 60;
+//        second = second - hour * 3600 - minute * 60;
+//        if (hour < 10) {
+//            if (minute < 10) {
+//                if (second < 10) {
+//                    return "0" + hour + ":0" + minute + ":0" + second;
+//                }
+//                return "0" + hour + ":0" + minute + ":" + second;
+//            }
+//            if (second < 10) {
+//                return "0" + hour + minute + ":0" + second;
+//            }
+//            return "0" + hour + minute + ":" + second;
+//        }
+//        if (minute < 10) {
+//            if (second < 10) {
+//                return hour + ":0" + minute + ":0" + second;
+//            }
+//            return hour + ":0" + minute + ":" + second;
+//        }
+//        if (second < 10) {
+//            return hour + minute + ":0" + second;
+//        }
+//        return hour + minute + ":" + second;
     }
 }
