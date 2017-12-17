f63f1f8350a8ea41c9e13782bab0e58e123dfac9
五爷 <164660718@qq.com>
2017-12-17 22:02:04 +0800

    添加飞行参数设置逻辑

--- app/src/main/java/com/dji/FPVDemo/FCActivity.java
+++ app/src/main/java/com/dji/FPVDemo/FCActivity.java
@@ -5,19 +5,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dji.FPVDemo.util.DJIUtils;
+import com.dji.FPVDemo.view.ActionSheetDialog;
 import com.dji.FPVDemo.view.SwitchButton;
 
 import dji.common.error.DJIError;
 import dji.common.flightcontroller.DJIFlightFailsafeOperation;
 import dji.common.util.DJICommonCallbacks;
 import dji.sdk.flightcontroller.DJIFlightController;
+import dji.sdk.flightcontroller.DJIIntelligentFlightAssistant;
 import dji.sdk.products.DJIAircraft;
 
 
@@ -32,14 +36,15 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
     private SwitchButton sbLed;
     private TextView tvFailSafe;
     private EditText etGoHomeThreshold;
-    private SwitchButton sbVisionLocation;
+    private SwitchButton sbVisionPosition;
 
     private DJIFlightController flightController;
 
     private float goHomeAltitude = 0;
-    private DJIFlightFailsafeOperation failSafe = DJIFlightFailsafeOperation.Hover;
+    private DJIFlightFailsafeOperation failSafeOperation = DJIFlightFailsafeOperation.Hover;
     private boolean ledSwitch = false;
     private int goHomeBatteryThreshold = 25;
+    private boolean visionPosition = false;
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -48,9 +53,10 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
                     String strAltitude = String.valueOf((int) (goHomeAltitude));
                     etGoHomeAltitude.setText(strAltitude);
                     sbLed.setChecked(ledSwitch);
-                    int operationId = DJIUtils.getMapValue(DJIUtils.failSafeOperationMap, failSafe);
+                    int operationId = DJIUtils.getMapValue(DJIUtils.failSafeOperationMap, failSafeOperation);
                     tvFailSafe.setText(operationId);
                     etGoHomeThreshold.setText(String.valueOf(goHomeBatteryThreshold));
+                    sbVisionPosition.setChecked(visionPosition);
                     break;
             }
         }
@@ -81,11 +87,30 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
         });
 
         etGoHomeAltitude = (EditText) findViewById(R.id.et_go_home_height);
+        etGoHomeAltitude.setOnEditorActionListener(etListener);
         tvFailSafe = (TextView) findViewById(R.id.tv_fail_safe_operation);
+        tvFailSafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFailSafeOperationMenu();
+            }
+        });
 
         etGoHomeThreshold = (EditText) findViewById(R.id.et_smart_go_home_threshold);
+        etGoHomeThreshold.setOnEditorActionListener(etListener);
 
-        sbVisionLocation = (SwitchButton) findViewById(R.id.sb_vision_location);
+        sbVisionPosition = (SwitchButton) findViewById(R.id.sb_vision_location);
+        sbVisionPosition.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (flightController.getIntelligentFlightAssistant() != null) {
+                    flightController.getIntelligentFlightAssistant().setVisionPositioningEnabled(isChecked, null);
+                    visionPosition = isChecked;
+                } else {
+                    Toast.makeText(FCActivity.this, R.string.no_vision_position_hint, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         fetchDJIValue();
     }
@@ -112,7 +137,7 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
                 @Override
                 public void onSuccess(DJIFlightFailsafeOperation djiFlightFailsafeOperation) {
                     if (DJIUtils.failSafeOperationMap.containsKey(djiFlightFailsafeOperation)) {
-                        failSafe = djiFlightFailsafeOperation;
+                        failSafeOperation = djiFlightFailsafeOperation;
                     }
                     handler.sendEmptyMessage(MSG_FLIGHT_CONTROLLER_STATUS);
                 }
@@ -149,6 +174,22 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
                 }
             });
 
+            DJIIntelligentFlightAssistant assistant = flightController.getIntelligentFlightAssistant();
+            if (assistant != null) {
+                assistant.getVisionPositioningEnabled(new DJICommonCallbacks.DJICompletionCallbackWith<Boolean>() {
+
+                    @Override
+                    public void onSuccess(Boolean aBoolean) {
+                        visionPosition = aBoolean;
+                        handler.sendEmptyMessage(MSG_FLIGHT_CONTROLLER_STATUS);
+                    }
+
+                    @Override
+                    public void onFailure(DJIError djiError) {
+
+                    }
+                });
+            }
         }
     }
 
@@ -176,5 +217,61 @@ public class FCActivity extends FragmentActivity implements View.OnClickListener
                 break;
 
         }
+    }
+
+    private TextView.OnEditorActionListener etListener = new TextView.OnEditorActionListener() {
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            //按下确认的时候进行处理
+            if (EditorInfo.IME_ACTION_DONE == actionId) {
+                int id = v.getId();
+                if (id == R.id.et_go_home_height) {
+                    String strGoHomeHeight = etGoHomeAltitude.getText().toString();
+                    float height = Float.valueOf(strGoHomeHeight);
+                    if (height < 20 || height > 500) {
+                        Toast.makeText(FCActivity.this, getString(R.string.number_between_a_and_b, 20, 500), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                    flightController.setGoHomeAltitude(Float.valueOf(strGoHomeHeight), null);
+                } else if (id == R.id.et_smart_go_home_threshold) {
+                    String strGoHomeThreshold = etGoHomeThreshold.getText().toString();
+                    int goHomeThreshold = Integer.valueOf(strGoHomeThreshold);
+                    if (goHomeThreshold < 25 || goHomeThreshold > 50) {
+                        Toast.makeText(FCActivity.this, getString(R.string.number_between_a_and_b, 25, 50), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                    flightController.setGoHomeBatteryThreshold(goHomeThreshold, null);
+                }
+            }
+            return false;
+        }
+    };
+
+    private void showFailSafeOperationMenu() {
+
+        final int[] strResIds = new int[]{R.string.hover, R.string.landing, R.string.go_home};
+
+        ActionSheetDialog.OnSheetItemClickListener listener = new ActionSheetDialog.OnSheetItemClickListener() {
+
+            @Override
+            public void onClick(TextView v, int which) {
+                Object key = DJIUtils.getMapKey(DJIUtils.failSafeOperationMap, strResIds[which - 1]);
+                if (key != null && key instanceof DJIFlightFailsafeOperation) {
+                    DJIFlightFailsafeOperation operation = (DJIFlightFailsafeOperation) key;
+                    flightController.setFlightFailsafeOperation(operation, null);
+
+                    failSafeOperation = operation;
+                    Message msg = Message.obtain();
+                    msg.what = MSG_FLIGHT_CONTROLLER_STATUS;
+                    handler.sendMessage(msg);
+                }
+            }
+        };
+
+        new ActionSheetDialog(this).builder().setCancelable(true).setCanceledOnTouchOutside(true)
+                .addSheetItem(getString(strResIds[0]), ActionSheetDialog.SheetItemColor.Black, listener)
+                .addSheetItem(getString(strResIds[1]), ActionSheetDialog.SheetItemColor.Black, listener)
+                .addSheetItem(getString(strResIds[2]), ActionSheetDialog.SheetItemColor.Black, listener).show();
     }
 }
