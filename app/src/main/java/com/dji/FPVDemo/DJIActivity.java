4801cb4c21965f89fe939e0d90227e111ca03e85
五爷 <164660718@qq.com>
2017-12-19 23:55:27 +0800

    添加activity基类

---
+++ app/src/main/java/com/dji/FPVDemo/DJIActivity.java
@@ -0,0 +1,44 @@
+package com.dji.FPVDemo;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.v4.app.FragmentActivity;
+
+import com.dji.FPVDemo.dji.MessageType;
+
+
+public class DJIActivity extends FragmentActivity {
+
+    private FPVApplication app;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (FPVApplication) getApplication();
+    }
+
+    protected boolean sendDJIMessage(Message message) {
+        return app.sendDJIMessage(message);
+    }
+
+    public boolean registerDJIMessenger(int messageId, Messenger messenger) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("registerMessageId", messageId);
+        Message message = Message.obtain();
+        message.what = MessageType.MSG_REGISTER_MESSENGER;
+        message.replyTo = messenger;
+        message.setData(bundle);
+        return app.sendDJIMessage(message);
+    }
+
+    public boolean unregisterDJIMessenger(int messageId, Messenger messenger) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("registerMessageId", messageId);
+        Message message = Message.obtain();
+        message.what = MessageType.MSG_UNREGISTER_MESSENGER;
+        message.replyTo = messenger;
+        message.setData(bundle);
+        return app.sendDJIMessage(message);
+    }
+
+}
