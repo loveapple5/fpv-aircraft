26443aa2040ef1f118ccddf5958c44a30c021722
五爷 <164660718@qq.com>
2017-12-26 22:04:35 +0800

    添加flag参数

--- app/src/main/java/com/synseaero/dji/Task.java
+++ app/src/main/java/com/synseaero/dji/Task.java
@@ -8,10 +8,12 @@ public abstract class Task implements Runnable {
 
     protected Messenger messenger;
     protected Bundle data;
+    protected int flag = 0;
 
     public Task(Bundle data, Messenger messenger) {
         this.data = data;
         this.messenger = messenger;
+        flag = data.getInt("flag", 0);
     }
 
 }
